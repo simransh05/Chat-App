48d0fe0c27b39a1c53a454b70cdc5822ea4546a8
Simran Sharma <ss1043422@gmail.com>
2025-12-19 14:11:49 +0530

    origin

--- Backend/Controller/auth.js
+++ Backend/Controller/auth.js
@@ -50,8 +50,8 @@ async function login(req, res) {
     );
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "Lax",
-      secure: false,
+      sameSite: 'None',
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000
     });
 
